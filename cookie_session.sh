694f703caa7a358da63bd03747e8ca8b2173016c
John <johnnyw66@gmail.com>
2023-11-07 02:13:43 +0000

    Update cookie_session.sh

--- cookie_session.sh
+++ cookie_session.sh
@@ -1,4 +1,4 @@
 while [[ true ]]; do
   python3 start_session_from_cookies_win32.py $@
-  echo "**************** Completed Session `date` ****************"
+  echo "**************** Completed Session ****************"
 done
