aedb9ef4ca977583349d2c8f38c1eae191438c1c
John <johnnyw66@gmail.com>
2024-01-26 06:21:28 +0000

    Update testnotify.py

--- testnotify.py
+++ testnotify.py
@@ -1,8 +1,18 @@
 import configure
 import requests
 
-def announce_opportunities(announcement):
-    print(f"announce_opportunties: '{announcement}'")
+def voicemonkey_announce(announcement, speaker='annoucegroup'):
+    print(f"announce: '{announcement}'")
+    res = requests.get("https://api-v2.voicemonkey.io/announcement",
+            params = {
+            'token': configure.VOICEMONKEY_TOKEN,
+            'device': speaker, 
+            'text':announcement
+            }) 
+    print(f"VoiceMonkey: {res.text}")
+
+def notify_opportunities(announcement):
+    print(f"notify_opportunities: '{announcement}'")
     res = requests.post("https://api.notifymyecho.com/v1/NotifyMe",
             json = {
             'accessCode': configure.NOTIFICATIONS_TOKEN,
@@ -10,8 +20,10 @@ def announce_opportunities(announcement):
             }) 
     print(res.text)
 
+def announce_opportunities(announcement):
+    notify_opportunities(announcement):        
+    voicemonkey_announce(announcement,
 
 
-
-announce_opportunities('YOU HAVE OPPORTUNTIES AVAILABLE. MAKE HASTE!')
+announce_opportunities('YOU HAVE OPPORTUNTIES AVAILABLE. MAKE HASTE!', speaker='annoucegroup')
 
